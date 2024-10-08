3f4a525fa8e6c3ae7fa0873c6a66e277036caab0
Kalana-indula <kalanasmedia@outlook.com>
2024-10-08 16:05:59 +0530

    section 15

--- day 15/coffee machine/code.py
+++ day 15/coffee machine/code.py
@@ -6,8 +6,50 @@ coffee_machine={
     "money":100
 }
 
-print(coffee_machine["sugar"])
-print(coffee_machine)
+espresso={
+    "water":200,
+    "coffee":50,
+    "sugar":20,
+    "milk":0,
+    "price":2
+}
+
+latte={
+    "water":150,
+    "coffee":50,
+    "sugar":20,
+    "milk":20,
+    "price":4
+}
+
+cappuccino={
+    "water": 150,
+    "coffee": 50,
+    "sugar": 20,
+    "milk": 50,
+    "price": 4
+}
+
+def make_espresso():
+    coffee_machine["water"]-=espresso["water"]
+    coffee_machine["sugar"]-=espresso["sugar"]
+    coffee_machine["coffee"]-=espresso["coffee"]
+    coffee_machine["milk"]-=espresso["milk"]
+    coffee_machine["money"]+=espresso["price"]
+
+def make_cappuccino():
+    coffee_machine["water"]-=cappuccino["water"]
+    coffee_machine["sugar"]-=cappuccino["sugar"]
+    coffee_machine["coffee"]-=cappuccino["coffee"]
+    coffee_machine["milk"]-=cappuccino["milk"]
+    coffee_machine["money"]+=cappuccino["price"]
+
+def make_latte():
+    coffee_machine["water"] -= latte["water"]
+    coffee_machine["sugar"] -= latte["sugar"]
+    coffee_machine["coffee"] -= latte["coffee"]
+    coffee_machine["milk"] -= latte["milk"]
+    coffee_machine["money"] += latte["price"]
 
 def report():
     print(
@@ -15,8 +57,30 @@ def report():
         f"Milk : {coffee_machine["milk"]} \n"
         f"Sugar : {coffee_machine["sugar"]} \n"
         f"Coffee : {coffee_machine["coffee"]} \n"
-        f"Money : {coffee_machine["money"]} \n"
+        f"Money : $ {coffee_machine["money"]} \n"
     )
 
+# getting user inputs
 def get_user_input():
-    input("Enter preferred type L for latte , E for espresso , C for cappacino ")
+    type=input("Enter preferred type L for latte , E for espresso , C for cappuccino ")
+    return type.lower()
+
+def buy_coffee():
+    coffee_type=get_user_input()
+    if(coffee_type=='c'):
+        make_cappuccino()
+    elif(coffee_type=='l'):
+        make_latte()
+    elif(coffee_type=='e'):
+        make_espresso()
+    else:
+        print("Enter valid type")
+
+    print(coffee_machine)
+
+while(True):
+    buy_coffee()
+    power=input("Press 'Off' for turn off the machine : ")
+
+    if(power=="Off"):
+        break
